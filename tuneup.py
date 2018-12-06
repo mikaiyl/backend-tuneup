dec1a3975b611c49923116951103b0826406d809
= <mikaiyl.davis@icloud.com>
2018-12-05 23:26:28 -0500

    improved()

--- tuneup.py
+++ tuneup.py
@@ -6,11 +6,20 @@ __author__ = "???"
 
 import cProfile
 import pstats
+import timeit
 
 
 def profile(func):
     """A function that can be used as a decorator to measure performance"""
-    raise NotImplementedError("Complete this decorator function")
+    def new(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        result = func(*args, **kwargs)
+        pr.disable()
+        stats = pstats.Stats(pr).sort_stats('cumtime')
+        stats.print_stats()
+        return result
+    return new
 
 
 def read_movies(src):
@@ -20,6 +29,15 @@ def read_movies(src):
         return f.read().splitlines()
 
 
+def is_duplicate(title, movies):
+    """Case insensitive search within a list"""
+    for movie in movies:
+        if movie.lower() == title.lower():
+            return True
+    return False
+
+
+setup = '''
 def is_duplicate(title, movies):
     """Case insensitive search within a list"""
     for movie in movies:
@@ -39,14 +57,51 @@ def find_duplicate_movies(src):
     return duplicates
 
 
+def read_movies(src):
+    """Read a list of movie titles"""
+    print('Reading file: {}'.format(src))
+    with open(src, 'r') as f:
+        return f.read().splitlines()
+'''
+
+
+@profile
+def find_duplicate_movies(src):
+    """Returns a list of duplicate movies from a src list"""
+    movies = read_movies(src)
+    duplicates = []
+    while movies:
+        movie = movies.pop()
+        if is_duplicate(movie, movies):
+            duplicates.append(movie)
+    return duplicates
+
+
+@profile
+def find_duplicate_movies_improved(src):
+    """Returns a list of duplicate movies from a src list"""
+    movies = read_movies(src)
+    uniq = []
+    dups = []
+    for m in movies:
+        if m not in uniq:
+            uniq.append(m)
+        else:
+            dups.append(m)
+    return dups
+
+
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
     # YOUR CODE GOES HERE
+    timer = timeit.Timer('find_duplicate_movies(\'movies.txt\')',
+                         setup=setup)
+    return min(timer.repeat(repeat=7, number=5))
 
 
 def main():
     """Computes a list of duplicate movie entries"""
-    result = find_duplicate_movies('movies.txt')
+    result = find_duplicate_movies_improved('movies.txt')
     print('Found {} duplicate movies:'.format(len(result)))
     print('\n'.join(result))
 
